f1983b8339a1767b3eb49fbacd50f8d9c06a47f3
sai-myne <snm.tgi@gmail.com>
2023-08-16 14:23:08 +0800

    first commit setup webcam graphics

---
+++ main.py
@@ -0,0 +1,31 @@
+import cv2
+import os
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+imgBackground = cv2.imread('Resources/background.png')
+
+# Importing the mode images into a list
+folderModePath = 'Resources/Modes'
+modePathList = os.listdir(folderModePath)
+imgModeList = []
+for path in modePathList:
+    imgModeList.append(cv2.imread(os.path.join(folderModePath, path)))
+    
+
+
+while True:
+    success, img = cap.read()
+    
+    imgBackground[162:162+480, 55:55+640] = img
+    imgBackground[44:44+633, 808:808+414] = imgModeList[0]
+    
+    # cv2.imshow("Webcam", img)
+    cv2.imshow("Face Attendance", imgBackground)
+    cv2.waitKey(1)
+
+
+
+
